ee8b94303cbf5c1bcabf7247730f390855529996
Pirgosth <pirgosth74@gmail.com>
2019-12-08 15:58:56 +0100

    End of Hexagon fix

--- Assets/Scripts/HexagonMesh.cs
+++ Assets/Scripts/HexagonMesh.cs
@@ -6,20 +6,14 @@ public class HexagonMesh : MonoBehaviour
 {
     public Material material;
     public int radius = 1;
-    public float thickness = 0.2f;
     // Start is called before the first frame update
 
     Vector3[] getVertices()
     {
-        Vector3[] vertices = new Vector3[12];
+        Vector3[] vertices = new Vector3[6];
         for (int i = 0; i < 6; i++)
         {
             vertices[i] = radius * (new Vector3(Mathf.Cos(60.0f / 180.0f * Mathf.PI * i), Mathf.Sin(60.0f / 180.0f * Mathf.PI * i)));
-        }
-
-        for (int i = 0; i < 6; i++)
-        {
-            vertices[6 + i] = (radius - thickness) * (new Vector3(Mathf.Cos(60.0f / 180.0f * Mathf.PI * i), Mathf.Sin(60.0f / 180.0f * Mathf.PI * i)));
         }
         return vertices;
     }
@@ -29,7 +23,7 @@ public class HexagonMesh : MonoBehaviour
         Mesh mesh = new Mesh();
 
         mesh.vertices = getVertices();
-        mesh.triangles = new int[] { 0, 1, 6, 1, 6, 7, 1, 7, 8, 1, 2, 8, 2, 8, 9, 2, 3, 9, 3, 9, 10, 3, 4, 10, 5, 10, 11, 4, 5, 10, 0, 5, 11, 0, 6, 11 };
+        mesh.triangles = new int[] {0, 1, 4, 1, 2, 3, 1, 3, 4, 0, 4, 5};
 
         GetComponent<MeshFilter>().mesh = mesh;
         GetComponent<MeshRenderer>().material = material;
