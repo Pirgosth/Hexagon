bb5f8f52ba46a4acb57c958dac1323a974ba405e
Pirgosth <pirgosth74@gmail.com>
2019-12-08 13:45:12 +0100

    Added OpenedHexagon with Collider

--- Assets/Scripts/OpenHexagonMesh.cs
+++ Assets/Scripts/OpenHexagonMesh.cs
@@ -2,14 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shape : MonoBehaviour
+public class OpenHexagonMesh : MonoBehaviour
 {
     public Material material;
     public int radius = 1;
     public float thickness = 0.2f;
-    Vector3[] newVertices;
-    Vector2[] newUV;
-    int[] triangles;
     // Start is called before the first frame update
 
     Vector3[] getVertices()
@@ -27,20 +24,36 @@ public class Shape : MonoBehaviour
         return vertices;
     }
 
+    Vector2[] getPolyPoints(Vector3[] vertices)
+    {
+        Vector2[] points = new Vector2[12];
+        int[] order = new int[] {1, 7, 6, 11, 10, 9, 8, 2, 3, 4, 5, 0};
+        for(int i = 0; i<12; i++)
+        {
+            points[i] = vertices[order[i]];
+        }
+        return points;
+    }
+
     void Start()
     {
         Mesh mesh = new Mesh();
 
-        mesh.vertices = getVertices();
-        mesh.triangles = new int[] { 0, 1, 6, 1, 6, 7, 1, 7, 8, 1, 2, 8, 2, 8, 9, 2, 3, 9, 3, 9, 10, 3, 4, 10, 5, 10, 11, 4, 5, 10, 0, 5, 11, 0, 6, 11 };
+        var vertices = getVertices();
+
+        mesh.vertices = vertices;
+        mesh.triangles = new int[] { 0, 1, 6, 1, 6, 7, 2, 8, 9, 2, 3, 9, 3, 9, 10, 3, 4, 10, 5, 10, 11, 4, 5, 10, 0, 5, 11, 0, 6, 11 };
 
         GetComponent<MeshFilter>().mesh = mesh;
         GetComponent<MeshRenderer>().material = material;
+        GetComponent<PolygonCollider2D>().points = getPolyPoints(vertices);
     }
 
     // Update is called once per frame
     void Update()
     {
-        GetComponent<MeshFilter>().mesh.vertices = getVertices();
+        var vertices = getVertices();
+        GetComponent<MeshFilter>().mesh.vertices = vertices;
+        GetComponent<PolygonCollider2D>().points = getPolyPoints(vertices);
     }
 }
